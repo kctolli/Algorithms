c738073052ee79d2a01e4d3b20d34950c72b2626
kctolli <tolligithub@gmail.com>
2022-08-09 01:15:20 -0600

    Deploy --- 08/09/2022 01:15:20 AM

--- twoSum.test.js
+++ twoSum.test.js
@@ -3,7 +3,7 @@ const twoSum = require('./twoSum.js');
 
 describe('twoSum', () => {
 
-    it('test function', () => {
+    test('test case 1', () => {
         twoSum([2,7,11,15], 9).toEqual([0,1])
     });
 
