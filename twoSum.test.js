fe4fee60abf04f3fb9f611bc18167cbc4b303f58
kctolli <tolligithub@gmail.com>
2022-08-09 01:18:22 -0600

    Deploy --- 08/09/2022 01:18:22 AM

--- twoSum.test.js
+++ twoSum.test.js
@@ -4,7 +4,14 @@ const twoSum = require('./twoSum.js');
 describe('twoSum', () => {
 
     test('test case 1', () => {
-        twoSum([2,7,11,15], 9).toEqual([0,1])
+        expect(twoSum([2,7,11,15], 9)).toEqual([0,1]);
     });
 
+    test('test case 2', () => {
+        expect(twoSum([3,2,4], 6)).toEqual([1,2]);
+    });
+
+    test('test case 3', () => {
+        expect(twoSum([3,3], 6)).toEqual([0,1]);
+    });
 });
