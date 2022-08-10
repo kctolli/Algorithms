d62aace43356a169ca12bd84d303d735d72eb382
kctolli <kyle@tollivers.org>
2022-08-10 14:10:48 -0600

    Deploy --- 08/10/2022 02:10:48 PM

---
+++ longestCommonPrefix.js
@@ -0,0 +1,9 @@
+/** https://leetcode.com/problems/longest-common-prefix/
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+};
+
+module.exports = longestCommonPrefix;
---
+++ longestCommonPrefix.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('@jest/globals');
+const longestCommonPrefix = require('./containsDuplicate.js');
+
+describe('longestCommonPrefix', () => {
+
+    test('test case 1', () => {
+        expect(longestCommonPrefix(["flower","flow","flight"])).toEqual("fl");
+    });
+
+    test('test case 2', () => {
+        expect(longestCommonPrefix(["dog","racecar","car"])).toEqual("");
+    });
+
+    test('test case 3', () => { 
+        expect(longestCommonPrefix()).toEqual();
+    });
+
+    test('test case 4', () => {
+        expect(longestCommonPrefix()).toEqual();
+    });
+
+    test('test case 5', () => {
+        expect(longestCommonPrefix()).toEqual();
+    });
+});
