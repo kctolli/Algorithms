3fb963429bac97c4bf40199859a88023dc62a327
kctolli <kyle@tollivers.org>
2022-08-10 11:25:43 -0600

    ignore

---
+++ romanToInt.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
+};
+
+
+/** Const Maps for Roman Numerals
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+*/
+
+const intToRomanValue = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M'
+};
+
+const romanValueToInt = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+};
+
+module.export = romanToInt;
---
+++ romanToInt.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('@jest/globals');
+const romanToInt = require('./RomanToInt.js');
+
+describe('romanToInt', () => {
+
+    test('test case 1', () => {
+        expect(romanToInt("III")).toEqual(3);
+    });
+
+    test('test case 2', () => {
+        expect(romanToInt("LVIII")).toEqual(58);
+    });
+
+    test('test case 3', () => {
+        expect(romanToInt("MCMXCIV")).toEqual(1994);
+    });
+});
