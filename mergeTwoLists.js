04cb6e457923d4ff69782001725d3e6631ba8813
kctolli <tolligithub@gmail.com>
2022-08-10 19:40:21 -0600

    Deploy --- 08/10/2022 07:40:21 PM

---
+++ mergeTwoLists.js
@@ -0,0 +1,28 @@
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    if (list1.length === 0 && list2.length === 0) {
+        return [];
+    } else if (list1.length === 0) {
+        return list2;
+    } else if (list2.length === 0) {
+        return list1;
+    } else { 
+        let arr = list1.concat(list2)
+        arr.sort()
+        return arr
+    }    
+};
+
+module.exports = mergeTwoLists;
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
---
+++ mergeTwoLists.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('@jest/globals');
+const mergeTwoLists = require('./mergeTwoLists.js');
+
+describe('mergeTwoLists', () => {
+
+    test('test case 1', () => {
+        expect(mergeTwoLists([1,2,4], [1,3,4])).toEqual([1,1,2,3,4,4]);
+    });
+
+    test('test case 2', () => {
+        expect(mergeTwoLists([], [])).toEqual([]);
+    });
+
+    test('test case 3', () => {
+        expect(mergeTwoLists([], [0])).toEqual([0]);
+    });
+});
