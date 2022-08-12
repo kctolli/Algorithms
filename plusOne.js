8096d2ca98a64a2bc95dc6898614d3913393ec79
kctolli <tolligithub@gmail.com>
2022-08-11 23:57:12 -0600

    Deploy --- 08/11/2022 11:57:12 PM

--- plusOne.js
+++ plusOne.js
@@ -3,8 +3,19 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    digits[digits.length -1] += 1;
+    digits[digits.length - 1] += 1;
     console.log(digits);
+
+    for (let i = digits.length - 1; i >= 0; i--){
+        if (digits[0] >= 10) {
+            digits[0] -= 10;
+            digits.unshift(1);
+        } else if (digits[i] >= 10) {
+            digits[i] -= 10;
+            digits[i - 1] += 1;
+        }
+    }
+
     return digits;
 };
 
